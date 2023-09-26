126a560ea4b82f24c5ebffd21ca1b178e74d1f27
unknown <cybergaymer0@protonmail.com>
2023-09-26 18:24:06 +0300

    base1

--- Tests/Proj1Tests/MainWindow.xaml.cs
+++ Tests/Proj1Tests/MainWindow.xaml.cs
@@ -13,8 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Proj1
+namespace Proj1Tests
 {
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
