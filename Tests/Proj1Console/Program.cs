3af6d6cac8a19a520e785f6100579fc8f56fd9d9
user <cybergaymer0@protonmail.com>
2023-10-05 14:43:52 +0300

    query

--- Tests/Proj1Console/Program.cs
+++ Tests/Proj1Console/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,17 @@ namespace Proj1Console
 {
     internal class Program
     {
+        private const string data_url = @"https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
+            var client = new HttpClient();
+
+            // WebClient client = new WebClient();
+
+            var csv_str = client.GetAsync(data_url).Result;
+
+            Console.ReadLine();
+
         }
     }
 }
